e91b7f10424703d9e8ab0e58af526535d221eba2
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-01 19:41:58 +0100

    initial commit, displaying thumbnail

---
+++ lithopia_server/loader/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import sentinel2
+from django.template import loader
+from django.http import HttpResponse
+from io import BytesIO
+
+# Create your views here.
+def index(request):
+    template = loader.get_template('loader/main.html')
+    return HttpResponse(template.render({}, request))
+
+
+def quicklook(request):
+    location = (float(request.GET['lat']), float(request.GET['lon']))
+    max_cloud_cover = float(request.GET['cloud_cover'])
+
+    sentinel_response = sentinel2.sentinel_requests.get_latest(location)
+    entries = sentinel2.sentinel_requests.get_entries(sentinel_response)
+    entry = sentinel2.sentinel_requests.get_latest_with_cloud_limit(entries, max_cloud_cover)
+    thumbnail = sentinel2.sentinel_requests.quicklook(entry)
+    response = HttpResponse(content_type="image/jpeg")
+    thumbnail.save(response, 'jpeg')
+
+    return response
---
+++ lithopia_server/sentinel_loader/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SentinelLoaderConfig(AppConfig):
+    name = 'sentinel_loader'
