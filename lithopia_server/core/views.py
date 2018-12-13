6b0029edee9098e5c69d7e274367ac0157a620e7
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-13 13:23:35 +0100

    added difference to template barplots

--- lithopia_server/core/views.py
+++ lithopia_server/core/views.py
@@ -1,4 +1,9 @@
+import cv2
+
 from django.shortcuts import render
+
+import matplotlib
+matplotlib.use('Agg')
 
 from django.http import HttpResponse
 from .models import RequestImage, settings, ReferenceImage
@@ -11,6 +16,8 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.gridspec as gridspec
 
 HTML_DATE_FORMAT = '%d.%m.%Y %H:%M:%S'
 
@@ -94,6 +101,8 @@ def get_histogram(request, name):
     canvas.print_png(png_output)
     response = HttpResponse(png_output.getvalue(), content_type='image/png')
 
+    # plt.close(fig)
+
     return response
 
 
@@ -102,9 +111,72 @@ def create_reference(request):
     return HttpResponse("Processing request...")
 
 
+BARPLOT_Z_LIMIT = 100
+
+def format_3d_barplot(ax):
+    ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+    # make the grid lines transparent
+    ax.xaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+    ax.yaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+    ax.zaxis._axinfo["grid"]['color'] = (1, 1, 1, 0)
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    ax.set_zlim(0, BARPLOT_Z_LIMIT)
+
+
 def get_diff_image(request, name):
     processed_item = RequestImage.objects.filter(dataset__name=name)[0]
-    image = Image.open(processed_item.cropped_diff_image_path)
-    response = HttpResponse(content_type="image/"+RequestImage.IMAGES_FORMAT)
-    image.save(response, RequestImage.IMAGES_FORMAT)
+    image = cv2.imread(processed_item.cropped_diff_image_path)
+    fig = plt.figure(figsize=(12, 5))
+    gs = gridspec.GridSpec(2, 3)
+    plot_red = fig.add_subplot(gs[0,0], projection='3d')
+    plot_green = fig.add_subplot(gs[0,1], projection='3d')
+    plot_blue = fig.add_subplot(gs[1,0], projection='3d')
+    plot_average = fig.add_subplot(gs[1,1], projection='3d')
+    plot_image = fig.add_subplot(gs[:,2])
+    ## todo: verify that the ravel procedure is necessary
+    _x = range(0, image.shape[0])
+    _y = range(0, image.shape[1])
+    xx, yy = np.meshgrid(_x, _y)
+    x, y = xx.ravel(), yy.ravel()
+    bars_red = image[:,:,2].ravel()
+    bars_red[bars_red > BARPLOT_Z_LIMIT] = BARPLOT_Z_LIMIT
+    bars_green = image[:,:,1].ravel()
+    bars_green[bars_green > BARPLOT_Z_LIMIT] = BARPLOT_Z_LIMIT
+    bars_blue = image[:,:,0].ravel()
+    bars_blue[bars_blue > BARPLOT_Z_LIMIT] = BARPLOT_Z_LIMIT
+    bars_average = np.mean(image, axis=2).ravel()
+    bars_average[bars_average > BARPLOT_Z_LIMIT] = BARPLOT_Z_LIMIT
+    bottom = np.zeros_like(bars_red) # red, green, blue should be the same
+    plot_red.bar3d(x, y, bottom, 1, 1, bars_red, shade=True, color='red')
+    plot_green.bar3d(x, y, bottom, 1, 1, bars_green, shade=True, color='green')
+    plot_blue.bar3d(x, y, bottom, 1, 1, bars_blue, shade=True, color='blue')
+    plot_average.bar3d(x, y, bottom, 1, 1, bars_average, shade=True, color='white')
+    plot_image.imshow(image[:,:,::-1])
+    plot_image.axis('off')
+    format_3d_barplot(plot_red)
+    format_3d_barplot(plot_green)
+    format_3d_barplot(plot_blue)
+    format_3d_barplot(plot_average)
+
+    plt.subplots_adjust(
+        left=0.05,
+        bottom=0.05,
+        right=0.95,
+        top=0.95,
+        wspace=0.05,
+        hspace=0.05)
+
+    canvas = FigureCanvasAgg(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+
+    response = HttpResponse(png_output.getvalue(), content_type='image/png')
+
+    # plt.close(fig)
+
     return response
