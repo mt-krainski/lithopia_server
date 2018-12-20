5280ff33171a4ae42430fa71e6a111424fb17321
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-20 20:25:30 +0100

    removed urls from loader - not necessary

--- lithopia_server/loader/urls.py
+++ lithopia_server/loader/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('quicklook', views.quicklook, name='quicklook'),
-    path('download_latest', views.download_latest, name='download_latest'),
-    path('download_status', views.download_status, name='download_status'),
+    # path('', views.index, name='index'),
+    # path('quicklook', views.quicklook, name='quicklook'),
+    # path('download_latest', views.download_latest, name='download_latest'),
+    # path('download_status', views.download_status, name='download_status'),
 ]
