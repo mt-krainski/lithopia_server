ecfb0cf20c23e2c936af57957ded1f106078b9eb
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-20 22:38:43 +0100

    working on the setting and resetting utility

--- lithopia_server/core/management/commands/perform_update.py
+++ lithopia_server/core/management/commands/perform_update.py
@@ -1,5 +1,14 @@
 from django.core.management.base import BaseCommand, CommandError
 from core import tasks
+import sys
+import psutil
+import subprocess
+
+PYTHON_PROCESS_KEYWORD = 'python'
+PROCESS_TASKS_KEYWORD = 'process_tasks'
+
+MANAGE_SCRIPT = "manage.py"
+PROCESS_TASKS = "process_tasks"
 
 class Command(BaseCommand):
     help = 'Resets the tasks responsible for running dataset updates'
@@ -12,8 +21,38 @@ class Command(BaseCommand):
             help='Cleans the tasks before issuing a new one',
         )
 
+        parser.add_argument(
+            '--kill',
+            action='store_true',
+            dest='kill',
+            help='Cleans the tasks and kills all process_tasks processes',
+        )
+
     def handle(self, *args, **options):
+
+        process_tasks = self.get_process_tasks()
+
         if options['reset']:
+            process_tasks.kill()
+
+        if options['reset'] or not process_tasks:
             tasks.clean_perform_update()
+            subprocess.Popen([
+                sys.executable, MANAGE_SCRIPT, PROCESS_TASKS
+            ])
 
         tasks.add_update_task()
+
+    @staticmethod
+    def get_process_tasks():
+        python_processes = [
+            process for process in psutil.process_iter()
+                    if PYTHON_PROCESS_KEYWORD in process.name()
+        ]
+
+        for p_process in python_processes:
+            if p_process.exe() == sys.executable:
+                if PROCESS_TASKS_KEYWORD in p_process.cmdline():
+                    return p_process
+
+        return False
