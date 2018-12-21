ee0dcc23bc084316ae277adbb7cad372e976d720
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-21 17:42:14 +0100

    added view for setting copernicus datahub credentials

--- lithopia_server/home/urls.py
+++ lithopia_server/home/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('sentinel_credentials', views.sentinel_credentials, name='sentinel_credentials'),
+    path('update_sentinel_credentials', views.update_sentinel_credentials, name='update_sentinel_credentials'),
 ]
--- lithopia_server/home/views.py
+++ lithopia_server/home/views.py
@@ -1,11 +1,17 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from core.models import Dataset, RequestImage, ReferenceImage
+from django.views.decorators.csrf import csrf_protect
+from sentinel2 import credentials
+
+from core.models import Dataset, RequestImage, ReferenceImage, settings
 
 # Create your views here.
 
 TIMESTAMP_FORMAT = "%d.%m.%Y %H:%M:%S"
+USERNAME_TAG = "Copernicus_username"
+PASSWORD_TAG = "Copernicus_password"
 
 
 def home(request):
@@ -17,5 +23,29 @@ def home(request):
         "detected_len": detected.count(),
         "detected_last_stamp": detected_last_stamp,
         "processed_ratio": RequestImage.objects.count()/Dataset.objects.count()*100.0,
+        "lat": settings.target_lat,
+        "lon": settings.target_lon,
     }
     return render(request, 'home/home.html', context)
+
+
+@staff_member_required
+def sentinel_credentials(request):
+    return render(
+        request,
+        'home/sentinel_cred_form.html',
+        {
+            "username_tag": USERNAME_TAG,
+            "password_tag": PASSWORD_TAG,
+        })
+
+
+@staff_member_required
+def update_sentinel_credentials(request):
+    username = request.POST[USERNAME_TAG]
+    password = request.POST[PASSWORD_TAG]
+    if credentials.test_credentials(username, password):
+        credentials.store_credentials(username, password)
+        return HttpResponse("Credentials updated")
+    else:
+        return HttpResponse("Credentinals not valid!")
