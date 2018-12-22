dc9b8e7e4b17dafcac4a46b3aa5fa9b29918467e
Mateusz Kraiński <mt.krainski@gmail.com>
2018-12-22 17:22:47 +0100

    added view for getting time of earliest acquisition

--- lithopia_server/acquisition/urls.py
+++ lithopia_server/acquisition/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.acquisition_page, name='acquisition_page'),
+    path('earliest', views.earliest, name='get_earliest')
 ]
--- lithopia_server/acquisition/views.py
+++ lithopia_server/acquisition/views.py
@@ -1,11 +1,31 @@
+import json
+from datetime import datetime, date
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from pytz import utc
+
 from .models import Acquisition
 
 
 # Create your views here.
+def datetime_parser(o):
+    if isinstance(o, (date, datetime)):
+        return o.isoformat()
 
 def acquisition_page(request):
-    return render(request,
-                  'acquisition/acquisition_summary.html',
-                  {'acquisition_list': Acquisition.objects.order_by('observation_time_start')})
+    return render(
+        request,
+      'acquisition/acquisition_summary.html',
+      {'acquisition_list':
+           Acquisition.objects.order_by('observation_time_start')})
+
+def earliest(request):
+    earliest = Acquisition.objects\
+        .filter(observation_time_start__gt=datetime.now().replace(tzinfo=utc))\
+        .order_by('observation_time_start')[0]
+
+    return HttpResponse(
+        json.dumps(earliest.observation_time_start, default=datetime_parser),
+        'application/json'
+    )
