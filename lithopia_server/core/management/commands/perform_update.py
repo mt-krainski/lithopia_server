e1bec5f4490ad1d88a5a0af379160e0579fd4aad
Mateusz Kraiński <mt.krainski@gmail.com>
2019-01-03 20:05:51 +0100

    added extensive log to process_tasks script

--- lithopia_server/core/management/commands/perform_update.py
+++ lithopia_server/core/management/commands/perform_update.py
@@ -13,7 +13,9 @@ PYTHON_PROCESS_KEYWORD = 'python'
 PROCESS_TASKS_KEYWORD = 'process_tasks'
 
 MANAGE_SCRIPT = os.path.join(BASE_DIR, "manage.py")
-PROCESS_TASKS = "process_tasks"
+PROCESS_TASKS = f"process_tasks --duration {3*60*60}" # run for 3h max, cronjob is scheduled every 2 hrs
+
+logging.basicConfig(filename='~/lithopia_server.log', filemode='a', format='%[(asctime)s] %(process)d - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
 class Command(BaseCommand):
     help = 'Resets the tasks responsible for running dataset updates'
@@ -35,35 +37,57 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        logging.debug("Starting script.")
+
         process_tasks = self.get_process_tasks()
 
+        logging.debug(f"Got task: {process_tasks}")
+
         if options['kill']:
-            while process_tasks:
-                process_tasks.kill()
-                process_tasks = self.get_process_tasks()
+            logging.debug(f"Killing.")
+            for instance in process_tasks:
+                logging.debug(f"Task: {process_tasks}")
+                instance.kill()
+
+            logging.debug(f"Performing clean update")
 
             tasks.clean_perform_update()
             return
 
         if options['reset'] or not process_tasks:
 
-            while process_tasks:
-                process_tasks.kill()
-                process_tasks = self.get_process_tasks()
+            logging.debug(f"Resetting")
 
+            logging.debug(f"Killing.")
+            for instance in process_tasks:
+                logging.debug(f"Task: {process_tasks}")
+                instance.kill()
+
+            logging.debug(f"Performing clean update")
             tasks.clean_perform_update()
+
+            logging.debug(f"Running '{sys.executable} {MANAGE_SCRIPT} {PROCESS_TASKS}'")
             subprocess.Popen(
                 [sys.executable, MANAGE_SCRIPT, PROCESS_TASKS],
             )
+
+        logging.debug(f"Adding task")
 
         tasks.add_update_task()
 
     @staticmethod
     def get_process_tasks():
+
+        logging.debug(f"Getting process_task instances")
+
         python_processes = [
             process for process in psutil.process_iter()
                     if PYTHON_PROCESS_KEYWORD in process.name()
         ]
+
+        logging.debug(f"Found {len(python_processes)} python processes")
+
+        process_tasks_exec = []
 
         for p_process in python_processes:
             executable_name = [sys.executable]
@@ -72,8 +96,11 @@ class Command(BaseCommand):
                 executable_name.append(executable_name[0].replace(version, ""))
             else:
                 executable_name.append(executable_name[0]+version)
+
             if p_process.exe() in executable_name:
                 if PROCESS_TASKS_KEYWORD in p_process.cmdline():
-                    return p_process
+                    process_tasks_exec.append(p_process)
 
-        return False
+        logging.debug(f"Found {len(process_tasks_exec)} process_task processes")
+
+        return process_tasks_exec
